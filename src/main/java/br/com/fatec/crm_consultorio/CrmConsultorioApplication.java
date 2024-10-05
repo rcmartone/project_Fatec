e5ec4863850ac09eb74e26e4d660ff0ab18afd3f
Rafael Martone <rafaelcmartone@gmail.com>
2024-10-05 18:43:26 -0300

    Inicio projeto Spring

---
+++ src/main/java/br/com/fatec/crm_consultorio/CrmConsultorioApplication.java
@@ -0,0 +1,13 @@
+package br.com.fatec.crm_consultorio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmConsultorioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmConsultorioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fatec/crm_consultorio/CrmConsultorioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fatec.crm_consultorio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmConsultorioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
