b38048d635b5c001e11c25a7d2712e7ac5f8d167
Rafael Martone <rafaelcmartone@gmail.com>
2024-10-06 13:24:08 -0300

    Mudança do nome do projeto springboot

---
+++ src/main/java/br/com/projeto/projeto_fatec/ProjetoFatecApplication.java
@@ -0,0 +1,13 @@
+package br.com.projeto.projeto_fatec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoFatecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoFatecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/projeto/projeto_fatec/ProjetoFatecApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projeto.projeto_fatec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoFatecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
